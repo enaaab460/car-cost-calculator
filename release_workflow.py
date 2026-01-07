08322cbd9caba3d68347b2c040310e985c8575da
enaaab460 <34572749+enaaab460@users.noreply.github.com>
2026-01-07 16:12:57 +0200

    workflow

--- release_workflow.py
+++ release_workflow.py
@@ -51,7 +51,6 @@ def main():
     log_info("Preparing firefox branch...")
     run_command("git checkout firefox")
     run_command("python publish.py", input_text="y")
-    run_command('git commit -am "Release"',exit=False)
 
     log_info("Uploading firefox build to Firefox Add-ons...")
     if os.path.exists("web-ext-artifacts"):
@@ -61,6 +60,7 @@ def main():
     run_command('web-ext build -n source.zip --ignore-files build')
     run_command(f'web-ext sign --approval-timeout 0 -s build/firefox --upload-source-code web-ext-artifacts/source.zip --channel listed --api-key "{os.environ.get("WEB_EXT_API_KEY")}" --api-secret "{os.environ.get("WEB_EXT_API_SECRET")}"')
     shutil.rmtree("web-ext-artifacts")
+    run_command('git commit -am "Release"',exit=False)
 
     for branch in ["chrome", "online"]:
         log_info(f"Processing branch: {branch}")
@@ -82,7 +82,7 @@ def main():
             run_command(f'npx wrangler pages deploy build/online --branch=main --project-name "{os.environ.get("CLOUDFLARE_PROJECT")}"')
 
     # --- GitHub Release ---
-    log_info(f"[INFO] Creating GitHub Release...")
+    log_info(f"Creating GitHub Release...")
 
     # Resolve artifact paths
     artifact_files = glob.glob(os.path.join("build", "*.zip"))
