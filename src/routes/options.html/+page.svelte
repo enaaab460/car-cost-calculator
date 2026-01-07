cb057bdeee84ade3107dabfa69b167d9658fc943
enaaab460 <34572749+enaaab460@users.noreply.github.com>
2026-01-07 15:53:18 +0200

    check updates

--- src/routes/options.html/+page.svelte
+++ src/routes/options.html/+page.svelte
@@ -24,7 +24,7 @@
 
     onMount(async () => {
         // Load saved settings when the component mounts
-        let result = await chrome.storage.sync.get(['yearlyOdometer', 'haggle', 'typicalLife', 'selectorConfigs','currentyear', 'redFlags', 'vinProvider', 'alwaysSort','zipcode']) as any
+        let result = await chrome.storage.sync.get(['yearlyOdometer', 'haggle', 'typicalLife', 'selectorConfigs','currentyear', 'redFlags', 'vinProvider', 'alwaysSort']) as any
         if (result.yearlyOdometer) yearlyOdometer = result.yearlyOdometer;
         if (result.haggle) haggle = result.haggle;
         if (result.typicalLife) typicalLife = result.typicalLife;
@@ -126,6 +126,23 @@
         statusText = 'Settings and cars saved successfully.';
     }
 
+    async function checkUpdates(){
+        try {
+            let downloaded = await (await fetch("https://raw.githubusercontent.com/enaaab460/car-cost-calculator/refs/heads/firefox/static/sample-settings.json")).json()
+            if (downloaded && downloaded.settings){
+                console.log(downloaded)
+                if (JSON.stringify(downloaded.settings) == JSON.stringify($state.snapshot(selectorConfigs))){
+                    statusText = "Already using latest update"
+                } else {
+                    selectorConfigs = downloaded.settings
+                    statusText = "Updated website selectors, save to confirm changes."
+                }
+            }
+        }catch{
+            statusText = "Failed to update Website selectors"
+        }
+    }
+
     function restoredSettings(){
         const input = document.createElement('input');
         input.type = 'file';
@@ -215,6 +232,7 @@
     {/each}
 
     <button onclick={addSite}>Add New Site</button>
+    <button onclick={checkUpdates}>Check Updates</button>
 
     <hr />
 
